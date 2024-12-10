0fd57cc245113b1488573119f92d70b12bd4f711
Aleksa2024 <solevic.aleksa@yahoo.com>
2024-12-10 15:56:06 +0100

    Work in progress

---
+++ assets/js/api.js
@@ -0,0 +1,14 @@
+async function getProducts() {
+  try {
+    const response = await fetch("https://fakestoreapi.com/products");
+    const data = await response.json();
+    // console.log(data);
+    // allProducts = data;
+    return data;
+   
+  } catch (error) {
+    console.log("error", error);
+  }
+}
+
+export {getProducts}
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,70 @@
-async function getProducts() {
-  try {
-    const response = await fetch("https://fakestoreapi.com/products");
-    const products = await response.json();
-    console.log(products);
-  } catch (error) {
-    console.log("error", error);
-  }
+// const filterCategories = document.querySelector(".filter");
+import { getProducts } from "./api.js";
+const filterSelectEl = document.getElementById("filter-select");
+
+const sortSelectEl = document.getElementById("sort-select");
+
+const itemsContainerEl = document.getElementById("items-container");
+
+let compare;
+compare =
+  sortSelectEl.value === "ascending"
+    ? (compare = (a, b) => a.price - b.price)
+    : (compare = (a, b) => b.price - a.price);
+
+
+let allProducts = [];
+
+async function displayProducts() {
+  const products = await getProducts();
+  const productsList = products
+    .sort(compare)
+    .filter((product) =>
+      filterSelectEl.value !== "all"
+        ? product.category === filterSelectEl.value
+        : product
+    )
+    .map(
+      (item) =>
+        `<div class= "items">
+      <img class"product-img" src="${item.image}" alt = '${item.title}' width = "150" height = "175" />
+    <h4>${item.title}</h4>
+    <p>${item.description}</p>
+    <p> Price: $${item.price}</p>
+    <button>Add to cart</button>
+    </div>
+        `
+    )
+    .join("");
+  itemsContainerEl.innerHTML = productsList;
+  console.log(compare);
 }
-getProducts();
+
+function filterByCategory(category) {
+  const filteredProducts = allProducts.filter(
+    (product) => product.category === category
+  );
+  displayProducts(filteredProducts);
+}
+
+function testFunc(category) {
+  if (category === "all") {
+    getProducts();
+  } else filterByCategory(category);
+}
+
+function sortFunc(sortOption) {
+  let sortedProducts = [...allProducts];
+
+  if (sortOption === "ascending") {
+    sortedProducts.sort((a, b) => a.price - b.price);
+  } else if (sortOption === "descending") {
+    sortedProducts.sort((a, b) => b.price - a.price);
+  }
+  displayProducts(sortedProducts);
+}
+displayProducts();
+
+filterSelectEl.addEventListener("change", displayProducts);
+
+sortSelectEl.addEventListener("change", displayProducts);
