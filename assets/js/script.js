3db33f95f5113518b1286f8beafcd29895d41504
Cribe Pencheff <cribe@self-made.se>
2024-12-10 18:08:32 +0100

    fix: Default value to sorting logic + loading/spinner

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,30 @@
-// const filterCategories = document.querySelector(".filter");
 import { getProducts } from "./api.js";
+
 const filterSelectEl = document.getElementById("filter-select");
-
 const sortSelectEl = document.getElementById("sort-select");
-
 const itemsContainerEl = document.getElementById("items-container");
-
+const loaderEl = document.getElementById("loader");
 
 async function displayProducts() {
   let compare;
-compare =
-  sortSelectEl.value === "ascending"
-    ? compare = (a, b) => a.price - b.price
-    : compare = (a, b) => b.price - a.price;
+  switch (sortSelectEl.value) {
+    case "ascending":
+      compare = (a, b) => a.price - b.price;
+      break;
+    case "descending":
+      compare = (a, b) => b.price - a.price;
+      break;
+    case "none":
+    default:
+      compare = null;
+  }
 
+  loaderEl.classList.remove("hide");
+  itemsContainerEl.classList.add("hide");
 
   const products = await getProducts();
   const productsList = products
-    .sort(compare)
+    .sort(compare || ((a, b) => 0))
     .filter((product) =>
       filterSelectEl.value !== "all"
         ? product.category === filterSelectEl.value
@@ -25,22 +32,21 @@ compare =
     )
     .map(
       (item) =>
-        `<div class= "items">
-      <img class"product-img" src="${item.image}" alt = '${item.title}' width = "150" height = "175" />
-    <h4>${item.title}</h4>
-    <p>${item.description}</p>
-    <p> Price: $${item.price}</p>
-    <button>Add to cart</button>
-    </div>
-        `
+        `<div class="items">
+          <img class="product-img" src="${item.image}" alt="${item.title}" width = "150" height = "175" />
+          <h4>${item.title}</h4>
+          <p>${item.description}</p>
+          <p> Price: $${item.price}</p>
+          <button>Add to cart</button>
+        </div>`
     )
     .join("");
-  itemsContainerEl.innerHTML = productsList;
 
+  itemsContainerEl.innerHTML = productsList;
+  itemsContainerEl.classList.remove("hide");
+  loaderEl.classList.add("hide");
 }
 
 displayProducts();
-
 filterSelectEl.addEventListener("change", displayProducts);
-
 sortSelectEl.addEventListener("change", displayProducts);
